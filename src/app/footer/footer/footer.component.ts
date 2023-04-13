4a9df9d4ba1cb1ca4c2aa65b1dc71be8830196dc
ivantorale <ivantorale@campus.monlau.com>
2023-04-13 18:00:50 +0200

    13-04(definitivo)

--- src/app/footer/footer/footer.component.ts
+++ src/app/footer/footer/footer.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+  date : number = new Date().getFullYear()
 
+   getCurrentYear(): number {
+    const now = new Date();
+    return now.getFullYear();
+  }
 }
