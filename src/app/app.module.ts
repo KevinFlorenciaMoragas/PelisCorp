438ca5bf4e72aea1d502512d9e6e4b5d51ce8ce8
sergifercla <101187391+sergifercla@users.noreply.github.com>
2023-04-17 16:23:22 +0200

    saa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
- import { FooterComponent } from './footer/footer/footer.component';
+import { FooterComponent } from './footer/footer/footer.component';
 import { HeaderComponent } from './header/header/header.component';
 import { PrincipalComponent } from './principal/principal/principal.component';
 import { FilmsComponent } from './list/films/films.component';
+import { FilmViewComponent } from './film-view/film-view.component';
 
 import { RegisterComponent } from './user/register/register.component';
 import { LoginComponent } from './user/login/login.component';
 import { Injectable } from '@angular/core';
-import { FilmViewComponent } from './film-view/film-view.component';
 import { HttpClientModule } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Service } from './services/service.service';
 import { InterfacesComponent } from './interfaces/interfaces/interfaces.component';
 @NgModule({
@@ -23,11 +23,12 @@ import { InterfacesComponent } from './interfaces/interfaces/interfaces.componen
     RegisterComponent,
     LoginComponent,
     HeaderComponent,
-    FooterComponent,
     PrincipalComponent,
     FilmsComponent,
     FilmViewComponent,
-    InterfacesComponent
+    FooterComponent
+
+
 
   ],
   imports: [
