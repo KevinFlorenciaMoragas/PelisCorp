326111d5dd05d8cc712d9e02e899f63ee7586f90
Sergirojnav <101187276+Sergirojnav@users.noreply.github.com>
2023-04-25 16:05:45 +0200

    25/04/2023

--- src/app/principal/principal/principal.component.ts
+++ src/app/principal/principal/principal.component.ts
@@ -1,27 +1,26 @@
-import { Component } from '@angular/core';
-import { Movies } from 'src/app/interfaces/interfaces.component';
-import { MoviesService } from 'src/app/services/movies.service';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { OnInit } from '@angular/core';
+import { Component } from '@angular/core'; 
+import { Movies } from 'src/app/interfaces/interfaces.component'; 
+import { MoviesService } from 'src/app/services/movies.service'; 
+import { HttpClient } from '@angular/common/http'; 
+import { Observable } from 'rxjs'; import { OnInit } from '@angular/core'; 
+@Component({ 
+  selector: 'app-principal', 
+  templateUrl: './principal.component.html', 
+  styleUrls: ['./principal.component.css'] 
+}) 
 
-@Component({
-  selector: 'app-principal',
-  templateUrl: './principal.component.html',
-  styleUrls: ['./principal.component.css']
-})
-export class PrincipalComponent {
-  constructor(private http: MoviesService) { }
+export class PrincipalComponent { 
+  constructor(private http: MoviesService){ } 
   movies: Movies[] = []
-
-  ngOnInit() {
-    this.getAllMovies()
+    ngOnInit() {
+       this.getAllMovies() 
+      } 
+    
+    getAllMovies() { 
+      this.http.listAllMovies().subscribe(data => { 
+        console.log(data) 
+        this.movies = data as Movies[] 
+        console.log(this.movies) 
+      }) 
+    } 
   }
-  getAllMovies() {
-    this.http.listAllMovies().subscribe(data => {
-      console.log(data)
-      this.movies = data as Movies[]
-      console.log(this.movies)
-    })
-  }
-}
