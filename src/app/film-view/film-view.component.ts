c67701647817d24e9b8743525354272afafbc2d9
ivantorale <ivantorale@campus.monlau.com>
2023-05-12 17:58:08 +0200

    17:58

--- src/app/film-view/film-view.component.ts
+++ src/app/film-view/film-view.component.ts
@@ -17,35 +17,13 @@ export class FilmViewComponent {
   constructor(private http: MoviesService, private route: ActivatedRoute) {
 
   } 
-  movie: Movies = { 
-   id: 0, movieName: '', 
-   releaseDate: new Date(), 
-   plot: '', duration: 0, 
-   income: 0, 
-   score: 0, 
-   photo: '', 
-   favorites: [], 
-   director: [], 
-   actors: [], 
-   genres: [], 
-   awards: [], 
-   reviews: [], 
-   screenwritters: [] 
- } 
+
  genres: Genre[] = [] 
  movieId: any 
  id: any 
  
  ngOnInit() {
-   console.log("Estoy en ngOnInit") 
-   const movieIdParam = this.route.snapshot.paramMap.get('id'); console.log(movieIdParam) 
-   if (movieIdParam !== null) { 
-     this.id = +movieIdParam; 
-     console.log(this.id) 
-   } 
-   this.http.getMovieById(this.id).subscribe(data => { 
-     this.movie = data; }) 
-     //this.getAllMovies(), // this.getAllGenres() }
+
    }
 
 
