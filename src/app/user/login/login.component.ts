b35f5f8cb2b9a00792ca9662c53f2e32b196906c
KevinFlorenciaMoragas <120371012+KevinFlorenciaMoragas@users.noreply.github.com>
2023-05-23 17:03:06 +0200

    23/05

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -24,15 +24,21 @@ export class LoginComponent implements OnInit {
     let username = this.loginForm.value.username
     this.http.login(this.loginForm.value).subscribe(response => {
       this.router.navigate(['/'])
+    }, catchError => {
+      console.log(catchError)
+      if(catchError.status == 401){
+        alert("Usuario o contraseña incorrectos")
+      }
     })
+
     this.http.getUserByUsername(username).subscribe(data => {
       localStorage.setItem('role', data.role)
       console.log(data)
     }
     )
   }
-  username: FormControl = new FormControl<string | null>("", { /*validators: [Validators.required, Validators.minLength(5), Validators.maxLength(50)]*/ })
-  password: FormControl = new FormControl<string | null>("", {/* validators: [Validators.required, Validators.minLength(5), Validators.maxLength(100)]*/ })
+  username: FormControl = new FormControl<string | null>("", { validators: [Validators.required, Validators.minLength(5), Validators.maxLength(50)] })
+  password: FormControl = new FormControl<string | null>("", { validators: [Validators.required, Validators.minLength(5), Validators.maxLength(100)] })
 
   loginForm: FormGroup = new FormGroup({
     username: this.username,
